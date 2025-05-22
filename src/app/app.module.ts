36864087b768ef5bb9c9f7170d64ff280db6173d
Neo Thobela <neo.thobela0002@gmail.com>
2025-05-22 12:09:38 +0200

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TranslatorComponent } from './components/translator/translator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TranslatorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/translator/translator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-translator',
+  standalone: false,
+  templateUrl: './translator.component.html',
+  styleUrl: './translator.component.scss'
+})
+export class TranslatorComponent {
+
+}
