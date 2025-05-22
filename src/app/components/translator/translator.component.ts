0ee783bbc5d94d1aa5b79679ac086ca7f685d555
Neo Thobela <neo.thobela0002@gmail.com>
2025-05-22 19:53:10 +0200

    added morse code translator

--- src/app/components/translator/translator.component.ts
+++ src/app/components/translator/translator.component.ts
@@ -8,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class TranslatorComponent {
 
+  inputText: string = '';
+  translatedText: string = '';
+
+  morseCodeMap: { [key: string]: string } = {
+    A: '.-', B: '-...', C: '-.-.', D: '-..', E: '.', F: '..-.', G: '--.',
+    H: '....', I: '..', J: '.---', K: '-.-', L: '.-..', M: '--', N: '-.',
+    O: '---', P: '.--.', Q: '--.-', R: '.-.', S: '...', T: '-', U: '..-',
+    V: '...-', W: '.--', X: '-..-', Y: '-.--', Z: '--..',
+    0: '-----', 1: '.----', 2: '..---', 3: '...--', 4: '....-',
+    5: '.....', 6: '-....', 7: '--...', 8: '---..', 9: '----.',
+    ' ': '/', '!': '-.-.--', '?': '..--..', '.': '.-.-.-', ',': '--..--',
+    '-': '-....-', '_': '..--.-'
+  };
+
+  convertToMorseCode() {
+    this.translatedText = this.inputText
+      .toUpperCase()
+      .split('')
+      .map(char => this.morseCodeMap[char] || '')
+      .join(' ');
+  }
+  
 }
